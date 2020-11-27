8771884a908a53dee78a23f67ed230b6273aab05
yangpado <70185607+yangpado@users.noreply.github.com>
2020-11-27 15:23:28 +0900

    JS - navbar menu click

--- main.js
+++ main.js
@@ -12,3 +12,19 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// Handel scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click',(event) => {
+    
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return
+    }
+
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+    
+});
