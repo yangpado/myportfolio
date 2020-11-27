0d482c6f1f99750a538d4a595280b9a018d08722
yangpado <70185607+yangpado@users.noreply.github.com>
2020-11-27 15:36:07 +0900

    JS - home contact btn

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-
 document.addEventListener('scroll', () => {
 
     if(window.scrollY > navbarHeight){
@@ -16,15 +15,22 @@ document.addEventListener('scroll', () => {
 // Handel scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar_menu');
 navbarMenu.addEventListener('click',(event) => {
-    
     const target = event.target;
     const link = target.dataset.link;
     if(link == null){
         return
     }
 
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: "smooth"});
-    
+    scrollIntoView(link);
 });
+
+// Handle click on Contact me button on home 
+const homeContactBtn = document.querySelector('.home_contact');
+homeContactBtn.addEventListener('click',() => {
+    scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
