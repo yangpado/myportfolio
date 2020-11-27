1874b7a2348316c40b2bb3ecf2bf62f76cdec8a1
yangpado <70185607+yangpado@users.noreply.github.com>
2020-11-27 18:25:07 +0900

    JS - works

--- main.js
+++ main.js
@@ -53,6 +53,28 @@ arrowUp.addEventListener('click',() => {
     scrollIntoView('#home');
 });
 
+//Project
+const workBtnContainer = document.querySelector('.work_categories');
+const projectContainer = document.querySelector('.work_projects');
+const projects = document.querySelector('.project');
+workBtnContainer.addEventListener('click',(e)=>{
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null) {
+        return;
+    }
+    console.log(filter);
+    projects.forEach((project) => {
+        console.log(project.dataset.type);
+        if(filter === '*' || filter === project.dataset.type){
+            project.classList.remove('invisible');
+        } else{
+            project.classList.add('invisible');
+        }
+    });
+    
+    
+});
+
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
